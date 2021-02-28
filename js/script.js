80619b7f9633cc91551a612d7c18acad2a50cedd
Yusuf Sallam <ysallam64@gmail.com>
2021-02-28 00:29:08 -0500

    Fixed Clock Error @ 12:00

--- js/script.js
+++ js/script.js
@@ -15,14 +15,45 @@ var clockUpdater = setInterval(myTimer, 1000);
 
 function myTimer() {
     var d = new Date();
-    var hours = d.getHours()
-    var minutes = d.getMinutes()
-    var seconds = d.getSeconds()
+    var hours = d.getHours();
+    var minutes = d.getMinutes();
+    var seconds = d.getSeconds();
     var night = false;
+    var day = new Array();
+    day[0] = "Sunday";
+    day[1] = "Monday";
+    day[2] = "Tuesday";
+    day[3] = "Wednesday";
+    day[4] = "Thursday";
+    day[5] = "Friday";
+    day[6] = "Saturday";
+    var day = day[d.getDay()]
+
+    var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+    var month = month[d.getMonth()];
+
+
 
     if (hours > 12) {
         hours = hours - 12;
+
         var night = true;
+    }
+
+    if (hours === 0) {
+        hours = 12;
     }
 
     if (minutes < 10){
@@ -34,10 +65,12 @@ function myTimer() {
     }
 
     if (night){
-        var t = hours + ":" + minutes + ":" + seconds + " p.m";
+        var t = day + ", " + month + " " + d.getDate() + " || " + hours + ":" + minutes + ":" + seconds + " p.m";
     } else {
-        var t = hours + ":" + minutes + ":" + seconds + " a.m";
+        var t = day + ", " + month + " " + d.getDate() + " || " + hours + ":" + minutes + ":" + seconds + " a.m";
     }
 
     document.getElementById("Clock").innerHTML = t;
 }
+
+
