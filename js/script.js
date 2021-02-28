c2c23b8c496b3203153c9d97025877a2681c8f9b
Yusuf Sallam <ysallam64@gmail.com>
2021-02-27 21:39:13 -0500

    Fixed Timer Bugs and Beautified

--- js/script.js
+++ js/script.js
@@ -16,15 +16,28 @@ var clockUpdater = setInterval(myTimer, 1000);
 function myTimer() {
     var d = new Date();
     var hours = d.getHours()
-
+    var minutes = d.getMinutes()
+    var seconds = d.getSeconds()
+    var night = false;
 
     if (hours > 12) {
         hours = hours - 12;
-        var t = hours + ":" + d.getMinutes() + ":" + d.getSeconds();
-    } else {
-        var t = hours + ":" + d.getMinutes() + ":" + d.getSeconds();
+        var night = true;
     }
 
+    if (minutes < 10){
+        var minutes = "0" + d.getMinutes();
+    }
+
+    if (seconds < 10){
+        var seconds = "0" + d.getSeconds();
+    }
+
+    if (night){
+        var t = hours + ":" + minutes + ":" + seconds + " p.m";
+    } else {
+        var t = hours + ":" + minutes + ":" + seconds + " a.m";
+    }
 
     document.getElementById("Clock").innerHTML = t;
 }
