78f1b87d0aaeca18db15a1cfffdd14b8f4d6ccf2
kna27 <krisharora27@gmail.com>
2021-08-22 19:20:14 -0400

    Add class context menu

--- js/addClasses.js
+++ js/addClasses.js
@@ -41,14 +41,42 @@ function renderClasses() {
             var c = document.createElement("input")
             c.type = "button"
             c.value = key
-            c.onclick = function () { removeClass(key) }
+            c.onclick = function () { showMenu(key) }
             classesContainer.appendChild(c);
         }
     }
 }
+var activeClass;
 
-function removeClass(className) {
-    delete classes[className];
+function showMenu(key) {
+    var menu = document.getElementById("contextMenu")
+    activeClass = key;
+    menu.style.display = 'block';
+}
+
+function exit() {
+    document.getElementById("contextMenu").style.display = "none";
+}
+
+function rename() {
+    let newName = prompt("Enter the new name for " + activeClass);
+    classes[newName] = classes[activeClass];
+    delete classes[activeClass];
+    exit();
+    saveClasses();
+    renderClasses();
+}
+
+function changeLink() {
+    classes[activeClass] = prompt("Enter the new link for " + activeClass);
+    exit();
+    saveClasses();
+    renderClasses();
+}
+
+function remove() {
+    delete classes[activeClass];
+    exit();
     saveClasses();
     renderClasses();
 }
