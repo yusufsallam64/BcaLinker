aae442a64319e7d061844fe781aa3f6fa164b233
kna27 <krisharora27@gmail.com>
2021-08-22 15:32:48 -0400

    Add class list boilerplate

---
+++ js/addClasses.js
@@ -0,0 +1,17 @@
+var classes = {};
+window.onload = (event) => {
+    // load all classes from localStorage
+};
+
+
+function addClass() {
+    var className = document.getElementById("className").value;
+    var schoologyLink = document.getElementById("schoologyLink").value;
+    alert(className + "  |  " + schoologyLink);
+    // add class to classes dict
+    //save to storage
+}
+
+function saveClasses() {
+    // save dict to local storage
+}
